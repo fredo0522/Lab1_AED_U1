3c6a458e409fd021b31f03ceaf34c281a542b51b
LuisAlfredo <luisalfredo9905@gmail.com>
2018-08-21 20:00:34 -0500

    Changes to the interface

--- src/main/java/com/JavaFX/Lab1AED/main/Main.java
+++ src/main/java/com/JavaFX/Lab1AED/main/Main.java
@@ -4,18 +4,17 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class Main extends Application{
-
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
 		Parent root = FXMLLoader.load(getClass().getResource("/view/window.fxml"));
 		Scene scene = new Scene(root);
 		primaryStage.setScene(scene);
+		primaryStage.setTitle("Laboratorio AED");
 		primaryStage.show();
 		
 	}
